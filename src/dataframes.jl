50e5406fddf2ee7129c9dbaf9708b81f388a9265
Mirko Bunse <mirko.bunse@cs.tu-dortmund.de>
2022-05-28 19:10:51 +0200

    Rename _to_pairs -> to_pairs

--- src/dataframes.jl
+++ src/dataframes.jl
@@ -1,12 +1,14 @@
 using DataFrames
 
 ranks_and_cliques(df::AbstractDataFrame, treatment::Symbol, observation::Symbol, outcome::Symbol; kwargs...) =
-    ranks_and_cliques(_to_pairs(df, treatment, observation, outcome)...; kwargs...)
+    ranks_and_cliques(to_pairs(df, treatment, observation, outcome)...; kwargs...)
 
 plot(df::AbstractDataFrame, treatment::Symbol, observation::Symbol, outcome::Symbol; kwargs...) =
-    plot(_to_pairs(df, treatment, observation, outcome)...; kwargs...)
+    plot(to_pairs(df, treatment, observation, outcome)...; kwargs...)
 
-_to_pairs(df::AbstractDataFrame, treatment::Symbol, observation::Symbol, outcome::Symbol) =
+to_pairs(df::AbstractDataFrame, treatment::Symbol, observation::Symbol, outcome::Symbol) =
     map(pairs(groupby(sort(df, observation), treatment))) do (key, group)
         String(key[treatment]) => group[!, outcome]
     end # map from treatment name to all outcomes, which appear in the same order
+
+@deprecate _to_pairs(df::AbstractDataFrame, x::Symbol, y::Symbol, z::Symbol) to_pairs(df, x, y, z)
--- test/2d.jl
+++ test/2d.jl
@@ -1,10 +1,13 @@
 @testset "2D" begin
     df = CSV.read("2d.csv", DataFrame)
     sequence = Pair{String, Vector{Pair{String, Vector}}}[]
-    push!(sequence, "A" => CriticalDifferenceDiagrams._to_pairs(
+    @test_deprecated CriticalDifferenceDiagrams._to_pairs(
+        df, :method, :dataset, :accuracy
+    ) # test that the old API still works but raises a warning
+    push!(sequence, "A" => CriticalDifferenceDiagrams.to_pairs(
         df, :method, :dataset, :accuracy
     ))
-    push!(sequence, "B" => CriticalDifferenceDiagrams._to_pairs(
+    push!(sequence, "B" => CriticalDifferenceDiagrams.to_pairs(
         df, :method, :dataset, :accuracy
     ))
     plot = CriticalDifferenceDiagrams.plot(sequence...; maximize_outcome=true)
